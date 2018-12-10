6089ca988dd8c309b63387010108fbdc8bcb287c
Elisei Timofte <timofte@unfuddle.com>
2018-12-10 06:28:38 +0200

    add basic structure for SignUp Page

--- exams-management-system/ClientApp/src/App.js
+++ exams-management-system/ClientApp/src/App.js
@@ -5,8 +5,9 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 import Login from './components/LoginPage/Login';
+import SignUp from './components/SignUp';
 import setAuthTokenApp from "./utils/setAuthToken";
-
+import ROUTES from './constants/routes'
 
 // Check for auth token
 if (localStorage.jwtToken) {
@@ -19,6 +20,7 @@ class App extends Component {
         return (
             <Switch>
                 <Route exact path='/login' component={Login} />
+                <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
                 <Layout>
                     <Route exact path='/' component={Home} />
                     <Route path='/counter' component={Counter} />
---
+++ exams-management-system/ClientApp/src/constants/routes.js
@@ -0,0 +1,11 @@
+const ROUTES = {
+  // LANDING: '/',
+  SIGN_UP: '/signup',
+  // SIGN_IN: '/signin',
+  // HOME: '/home',
+  // ACCOUNT: '/account',
+  // PASSWORD_FORGET: '/pw-forget',
+  // ADMIN: '/admin',
+}
+
+export default ROUTES
