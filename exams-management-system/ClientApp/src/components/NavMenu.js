d4b7f41e2366d869fa4f3d14904a08bc0f0cf9ec
Elisei Timofte <timofte@unfuddle.com>
2018-12-11 23:18:51 +0200

    temporally hide all routes except signin and signup pages

--- exams-management-system/ClientApp/src/components/NavMenu.js
+++ exams-management-system/ClientApp/src/components/NavMenu.js
@@ -15,21 +15,6 @@ export default props => (
     </Navbar.Header>
     <Navbar.Collapse>
       <Nav>
-        <LinkContainer to={'/'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
         <LinkContainer to={ROUTES.SIGN_IN}>
           <NavItem>
             <Glyphicon glyph='th-list' /> Sign In
