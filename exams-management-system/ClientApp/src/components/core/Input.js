fe3a3956baf01f33ba4aa14966db4f41c67ac1a2
Elisei Timofte <timofte@unfuddle.com>
2018-12-12 20:27:46 +0200

    move core component styles in a dedicated subdirectory

--- exams-management-system/ClientApp/src/components/core/Input.js
+++ exams-management-system/ClientApp/src/components/core/Input.js
@@ -1,6 +1,6 @@
 import React     from 'react';
 
-import '../../styles/css/Input.css'
+import '../../styles/css/core/Input.css'
 
 class FormInputText extends React.Component {
   constructor() {
--- exams-management-system/ClientApp/src/components/core/Paper.js
+++ exams-management-system/ClientApp/src/components/core/Paper.js
@@ -1,6 +1,6 @@
 import React     from 'react';
 import classnames from 'classnames'
-import '../../styles/css/Paper.css'
+import '../../styles/css/core/Paper.css'
 
 class Paper extends React.Component {
 
