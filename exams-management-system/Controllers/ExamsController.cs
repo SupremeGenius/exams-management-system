ea361e344a625d7ca69728e8a9638612aa852fb6
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2018-12-13 23:14:23 +0200

    Added the minimum-minimum requeirements for the controller

---
+++ exams-management-system/Controllers/ExamsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EMS.Business;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exams_management_system.Controllers
+{
+    [Route("api/exams")]
+    [ApiController]
+    public class ExamsController : ControllerBase
+    {
+        private readonly IExamService examService;
+
+        public ExamsController(IExamService examService) => this.examService = examService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetExams()
+        {
+            var exams = await this.examService.GetAll();
+
+            return Ok(exams);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CerateExam([FromBody] CreatingExamModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var examId = await this.examService.CreateNew(model);
+
+            return Ok(examId);
+        }
+    }
+}
