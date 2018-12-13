2834a831a9945b629446a7863f5322c926af3d1c
Elisei Timofte <timofte@unfuddle.com>
2018-12-13 07:17:08 +0200

    add CreatingExamModel.cs

---
+++ EMS.Business/Exam/CreatingExamModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EMS.Domain;
+using EMS.Domain.Entities;
+
+namespace EMS.Business
+{
+    public class CreatingExamModel
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Type { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public Course Course { get; set; }
+
+        [Required]
+        public Professor Professor { get; set; }
+    }
+}
