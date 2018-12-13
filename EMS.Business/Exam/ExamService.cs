c4bb853a562ae9dd5267f5f2597d740c4e99de77
Elisei Timofte <timofte@unfuddle.com>
2018-12-13 08:31:51 +0200

    implement ExamService

---
+++ EMS.Business/Exam/ExamService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EMS.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Business
+{
+  public sealed class ExamService : IExamService
+  {
+    private readonly IRepository repository;
+
+    public ExamService(IRepository repository) => this.repository = repository;
+
+    public async Task<Guid> CreateNew(CreatingExamModel newExam)
+    {
+        var exam = Exam.Create(
+            type     : newExam.Type,
+            date     : newExam.Date,
+            course   : newExam.Course,
+            professor: newExam.Professor);
+
+        await this.repository.AddNewAsync(exam);
+        await this.repository.SaveAsync();
+        
+        return exam.Id;
+    }
+
+    public Task<ExamDetailsModel> FindById(Guid Id) => AllExamDetails.SingleOrDefaultAsync(e => e.Id == Id);
+
+    public Task<List<ExamDetailsModel>> GetAll() => AllExamDetails.ToListAsync();
+
+    private IQueryable<ExamDetailsModel> AllExamDetails => this.repository.GetAll<Exam>()
+      .Select(e => new ExamDetailsModel
+      {
+        Id        = e.Id,
+        Type      = e.Type,
+        Date      = e.Date,
+        Course    = e.Course,
+        Professor = e.Professor
+      });
+  }
+}
---
+++ EMS.Business/Exam/IExamService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EMS.Business
+{
+  public interface IExamService
+    {
+        Task<List<ExamDetailsModel>> GetAll();
+
+        Task<ExamDetailsModel> FindById(Guid Id);
+
+        Task<Guid> CreateNew(CreatingExamModel newExam);
+    }
+}
