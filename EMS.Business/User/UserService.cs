ad380ce9505f6233ca3561dcf4eff87797005c46
Elisei Timofte <timofte@unfuddle.com>
2018-12-13 08:31:20 +0200

    unlink unused references and use body expresion for FindByEmail method

--- EMS.Business/User/UserService.cs
+++ EMS.Business/User/UserService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +7,7 @@ using EMS.Domain;
 
 namespace EMS.Business
 {
-    public sealed class UserService : IUserService
+  public sealed class UserService : IUserService
     {
         private readonly IRepository repository;
 
@@ -29,10 +28,7 @@ namespace EMS.Business
 
         public Task<List<UserDetailsModel>> GetAll() => GetAllUserDetails().ToListAsync();
 
-        public Task<UserDetailsModel> FindByEmail(string email)
-        {
-            return GetAllUserDetails().SingleOrDefaultAsync(s => s.Email == email);
-        }
+        public Task<UserDetailsModel> FindByEmail(string email) => GetAllUserDetails().SingleOrDefaultAsync(s => s.Email == email);
 
         private IQueryable<UserDetailsModel> GetAllUserDetails() => this.repository.GetAll<User>()
             .Select(c => new UserDetailsModel
