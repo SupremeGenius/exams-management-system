a14e6b0298bf7cd2274fe7da424b13a2d3470f8b
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2019-01-05 18:17:55 +0200

    Users (+login,register) unit tests refactorized

--- XUnitTestProject1/RegisterUnitTest.cs
+++ XUnitTestProject1/RegisterUnitTest.cs
@@ -10,20 +10,26 @@ namespace XUnitTestProject1
 {
     public class RegisterControllerTest
     {
+        private readonly CreatingUserModel newModel;
+        private readonly Mock<IUserService> mockRepo;
+        private readonly RegisterController controller;
+        private readonly Guid guid;
+
+        public RegisterControllerTest()
+        {
+            newModel = new CreatingUserModel();
+            mockRepo = new Mock<IUserService>();
+            controller = new RegisterController(mockRepo.Object);
+            guid = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
+        }
         [Fact]
         public async Task Given_CreateUser_When_ModelIsValid_Then_OkStatusCode()
         {
             // Arrange
-            var guid = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
-            var mockRepo = new Mock<IUserService>();
-            var user = new CreatingUserModel();
-            var id = Task.FromResult(guid);
-            mockRepo.Setup(u => u.CreateNew(user)).Returns(id);
-
-            var controller = new RegisterController(mockRepo.Object);
+            mockRepo.Setup(u => u.CreateNew(newModel)).Returns(Task.FromResult(guid));
 
             // Act
-            var result = await controller.CreateUser(user);
+            var result = await controller.CreateUser(newModel);
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
@@ -32,17 +38,11 @@ namespace XUnitTestProject1
         public async Task Given_CreateUser_When_ModelIsInvalid_Then_BadStatusCode()
         {
             // Arrange
-            var guid = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
-            var mockRepo = new Mock<IUserService>();
-            var user = new CreatingUserModel();
-            var id = Task.FromResult(guid);
-            mockRepo.Setup(u => u.CreateNew(user)).Returns(id);
-            
-            var controller = new RegisterController(mockRepo.Object);
+            mockRepo.Setup(u => u.CreateNew(newModel)).Returns(Task.FromResult(guid));
             controller.ModelState.AddModelError("email", "Required");
 
             // Act
-            var result = await controller.CreateUser(user);
+            var result = await controller.CreateUser(newModel);
 
             // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
@@ -52,16 +52,10 @@ namespace XUnitTestProject1
         [Fact]
         public async Task Given_CreateUser_When_ModelIsValid_Then_Status422UnprocessableEntity()
         {
-            var mockRepo = new Mock<IUserService>();
-            var user = new CreatingUserModel();
-            var userDetails= Task.FromResult(new UserDetailsModel());
-            mockRepo.Setup(u => u.FindByEmail(user.Email)).Returns(userDetails);
-
-            var controller = new RegisterController(mockRepo.Object);
+            mockRepo.Setup(u => u.FindByEmail(newModel.Email)).Returns(Task.FromResult(new UserDetailsModel()));
             
-
             // Act
-            var result = (StatusCodeResult) await controller.CreateUser(user);
+            var result = (StatusCodeResult) await controller.CreateUser(newModel);
 
             // Assert
             Assert.Equal(422, result.StatusCode);
--- XUnitTestProject1/UsersUnitTest.cs
+++ XUnitTestProject1/UsersUnitTest.cs
@@ -13,24 +13,32 @@ namespace XUnitTestProject1
 {
     public class UsersUnitTest
     {
-        [Fact]
-        public async Task Given_UpdateUser_When_ModelIsValid_Then_OkStatusCode()
-        {
-            // Arrange
-            var updateUserModel = new UpdateUserModel();
-            var mockRepo = new Mock<IUserService>();
+        private readonly UpdateUserModel updateUserModel;
+        private readonly Mock<IUserService> mockRepo;
+        private readonly UsersController controller;
+        private readonly User userModel;
 
+        public UsersUnitTest()
+        {
+            updateUserModel = new UpdateUserModel();
+            mockRepo = new Mock<IUserService>();
+            controller = new UsersController(mockRepo.Object);
+
+            Mapper.Reset();
             Mapper.Initialize(cfg =>
             {
                 cfg.CreateMap<UpdateUserModel, User>()
                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.NewPassword));
             });
-            var userModel = Mapper.Map<UpdateUserModel, User>(updateUserModel);
+            userModel = Mapper.Map<UpdateUserModel, User>(updateUserModel);
+        }
 
+        [Fact]
+        public async Task Given_UpdateUser_When_ModelIsValid_Then_OkStatusCode()
+        {
+            // Arrange
             mockRepo.Setup(u => u.UpdateAsync(It.IsAny<Guid>(), It.IsAny<User>(),It.IsAny<string>())).
                 Returns(Task.FromResult(true));
-
-            var controller = new UsersController(mockRepo.Object);
 
             //Act
             var result = await controller.UpdateUser(updateUserModel, It.IsAny<Guid>());
@@ -43,20 +51,10 @@ namespace XUnitTestProject1
         public async Task Given_UpdateUser_When_ModelIsValid_Then_NoContentStatusCode()
         {
             // Arrange
-            var updateUserModel = new UpdateUserModel();
-            var oldPassword = "";
-            var mockRepo = new Mock<IUserService>();
-            var id = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
-            var true_value = Task.FromResult(true);
-
-            var userModel = Mapper.Map<UpdateUserModel, User>(updateUserModel);
-
-            mockRepo.Setup(u => u.UpdateAsync(id, userModel, oldPassword)).Returns(true_value);
-
-            var controller = new UsersController(mockRepo.Object);
+            mockRepo.Setup(u => u.UpdateAsync(It.IsAny<Guid>(), userModel, "")).Returns(Task.FromResult(true));
 
             //Act
-            var result = await controller.UpdateUser(updateUserModel, id);
+            var result = await controller.UpdateUser(updateUserModel, It.IsAny<Guid>());
 
             //Arrange
             Assert.IsType<NoContentResult>(result);
@@ -66,21 +64,11 @@ namespace XUnitTestProject1
         public async Task Given_UpdateUser_When_ModelIsInValid_Then_BadModel()
         {
             // Arrange
-            var updateUserModel = new UpdateUserModel();
-            var oldPassword = "";
-            var mockRepo = new Mock<IUserService>();
-            var id = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
-            var true_value = Task.FromResult(true);
-
-            var userModel = Mapper.Map<UpdateUserModel, User>(updateUserModel);
-
-            mockRepo.Setup(u => u.UpdateAsync(id, userModel, oldPassword)).Returns(true_value);
-
-            var controller = new UsersController(mockRepo.Object);
+            mockRepo.Setup(u => u.UpdateAsync(It.IsAny<Guid>(), userModel, "")).Returns(Task.FromResult(true));
             controller.ModelState.AddModelError("password", "Required");
 
             //Act
-            var result = await controller.UpdateUser(updateUserModel, id);
+            var result = await controller.UpdateUser(updateUserModel, It.IsAny<Guid>());
 
             //Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
@@ -91,10 +79,7 @@ namespace XUnitTestProject1
         public async Task Given_DeleteUser_When_IdIsValid_Then_OkStatusCode()
         {
             //Arrange
-            var mockRepo = new Mock<IUserService>();
-            var true_result = Task.FromResult(true);
-            mockRepo.Setup(u => u.Delete(It.IsAny<Guid>())).Returns(true_result);
-            var controller = new UsersController(mockRepo.Object);
+            mockRepo.Setup(u => u.Delete(It.IsAny<Guid>())).Returns(Task.FromResult(true));
 
             //Act
             var result = await controller.DeleteUser(It.IsAny<Guid>());
@@ -107,10 +92,7 @@ namespace XUnitTestProject1
         public async Task Given_DeleteUser_When_IdIsValid_Then_Status409Conflict()
         {
             //Arrange
-            var mockRepo = new Mock<IUserService>();
-            var false_result = Task.FromResult(false);
-            mockRepo.Setup(u => u.Delete(It.IsAny<Guid>())).Returns(false_result);
-            var controller = new UsersController(mockRepo.Object);
+            mockRepo.Setup(u => u.Delete(It.IsAny<Guid>())).Returns(Task.FromResult(false));
 
             //Act
             var result =(StatusCodeResult)await controller.DeleteUser(It.IsAny<Guid>());
@@ -118,5 +100,6 @@ namespace XUnitTestProject1
             //Assert
             Assert.Equal(StatusCodes.Status409Conflict, result.StatusCode);
         }
+
     }
 }
