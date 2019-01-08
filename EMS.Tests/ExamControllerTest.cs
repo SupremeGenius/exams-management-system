c607c1d33b749317cd5c3b346347e22bd0566618
Elisei Timofte <timofte@unfuddle.com>
2019-01-08 09:09:13 +0200

    add test for creating new exam endpoint with valid model

--- EMS.Tests/ExamControllerTest.cs
+++ EMS.Tests/ExamControllerTest.cs
@@ -5,17 +5,19 @@ using exams_management_system.Controllers;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-
+using System;
 namespace EMS.Tests
 {
-    public class ExamControllerTest
+ public class ExamControllerTest
     {
+        private readonly CreatingExamModel createExamModel;
         private readonly UpdateExamModel updateExamModel;
         private readonly Mock<IExamService> mockRepo;
         private readonly ExamsController controller;
 
         public ExamControllerTest()
         {
+            createExamModel = new CreatingExamModel();
             updateExamModel = new UpdateExamModel();
             mockRepo = new Mock<IExamService>();
             controller = new ExamsController(mockRepo.Object);
@@ -33,6 +35,22 @@ namespace EMS.Tests
 
             //Arrange
             Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Given_CreateExam_When_ModelIsValid_Then_OkStatusCode()
+        {
+            // Arrange
+            var examGuid = new Guid("ef7e98df-26ed-4b21-b874-c3a2815d18ac");
+            var Id = Task.FromResult(examGuid);
+            mockRepo.Setup(e => e.CreateNew(createExamModel)).Returns(Id);
+
+            // Act
+            var Result = (OkObjectResult)await controller.CreateExam(createExamModel);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(Result);
+            Assert.Equal(Id.Result, Result.Value);
         }
     }
 }
